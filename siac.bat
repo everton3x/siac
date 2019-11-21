ec22bf6f28a00efd91d53b8112a0ec87ac49034a
Everton da Rosa <everton3x@gmail.com>
2019-11-21 16:03:54 -0300

    Correções no siap.bat para ser usado via PATH e acrescentado arquivo para teste

--- siac.bat
+++ siac.bat
@@ -2,7 +2,7 @@
 
 cls
 
-rem php -c php.ini .\siac.php pad:convert "teste.csv" "C:\Users\everton.INDEPENDENCIA\Documents\Prefeitura\2019\PAD\2019-10\pm\MES10" "C:\Users\everton.INDEPENDENCIA\Documents\Prefeitura\2019\PAD\2019-10\cm\MES10"
+cd %~dp0
 
 php -c php.ini siac.php %*
 
---
+++ test.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cls
+
+rem php -c php.ini .\siac.php pad:convert "teste.csv" "C:\Users\everton.INDEPENDENCIA\Documents\Prefeitura\2019\PAD\2019-10\pm\MES10" "C:\Users\everton.INDEPENDENCIA\Documents\Prefeitura\2019\PAD\2019-10\cm\MES10"
